82d19015894ee9a89948da54696c3eeb84aa83e7
vicebaraban <46491960+vicebaraban@users.noreply.github.com>
2021-01-05 19:47:30 +0300

    added some moving functional

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import sys
 import pygame
 import engine
-
-FPS = 60
-SIZE = WIDTH, HEIGHT = 800, 600
+import constants
 
 
 class Game:
@@ -17,11 +15,11 @@ class Game:
         self._init_camera()
 
     def _init_screen(self):
-        self.screen = pygame.display.set_mode(SIZE)
+        self.screen = pygame.display.set_mode(constants.RESOLUTION)
         pygame.display.set_caption('XYgame')
 
     def _init_player(self):
-        pass
+        self._character = engine.PlayerCharacter((0, 0))
 
     def _init_game_map(self):
         pass
@@ -40,13 +38,25 @@ class Game:
             self.screen.fill('black')
             # отрисовка
             # обновление
-            self.clock.tick(FPS)
+            self.clock.tick(constants.FPS)
             pygame.display.flip()
 
     def _process_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self._terminate()
+            if event.type == pygame.KEYDOWN:
+                self._move_events(event)
+
+    def _move_events(self, event):
+        if event.key == pygame.K_DOWN:
+            self._character.move(engine.MoveDirection.DOWN)
+        elif event.key == pygame.K_UP:
+            self._character.move(engine.MoveDirection.UP)
+        elif event.key == pygame.K_RIGHT:
+            self._character.move(engine.MoveDirection.RIGHT)
+        elif event.key == pygame.K_LEFT:
+            self._character.move(engine.MoveDirection.LEFT)
 
     def _terminate(self):
         pygame.quit()
