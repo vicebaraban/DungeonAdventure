830492b2adbc683d381baa5420e46441df93a13a
Levetator <ilevetator@yandex.ru>
2021-01-05 16:09:43 +0300

    upd main

--- main.py
+++ main.py
@@ -9,10 +9,30 @@ SIZE = WIDTH, HEIGHT = 800, 600
 class Game:
     def __init__(self):
         pygame.init()
-        pygame.display.set_caption('XYgame')
-        self.screen = pygame.display.set_mode(SIZE)
-        self.clock = pygame.time.Clock()
         self.running = False
+        self.clock = pygame.time.Clock()
+        self._init_screen()
+        self._init_player()
+        self._init_game_map()
+        self._init_camera()
+
+    def _init_screen(self):
+        self.screen = pygame.display.set_mode(SIZE)
+        pygame.display.set_caption('XYgame')
+
+    def _init_player(self):
+        pass
+
+    def _init_game_map(self):
+        pass
+
+    def _init_camera(self):
+        pass
+
+    def run(self):
+        self.running = True
+        self._main_loop()
+        self._terminate()
 
     def _main_loop(self):
         while self.running:
@@ -23,19 +43,14 @@ class Game:
             self.clock.tick(FPS)
             pygame.display.flip()
 
-    def run(self):
-        self.running = True
-        while self.running:
-            self._main_loop()
-
-    def _terminate(self):
-        pygame.quit()
-        sys.exit()
-
     def _process_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self._terminate()
+
+    def _terminate(self):
+        pygame.quit()
+        sys.exit()
 
 
 if __name__ == '__main__':
