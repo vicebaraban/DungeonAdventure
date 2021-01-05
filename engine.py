21d7bf3b4e5dcbb12cd34a2494341625326fff17
vicebaraban <46491960+vicebaraban@users.noreply.github.com>
2021-01-05 19:48:14 +0300

    updated character classes

--- engine.py
+++ engine.py
@@ -1,10 +1,60 @@
+import pygame
+from enum import Enum, auto
+import math_operations
+import constants
+
+
+class MoveDirection(Enum):
+    UP = auto()
+    DOWN = auto()
+    LEFT = auto()
+    RIGHT = auto()
+
+
 class Character:
-    def __init__(self, pos):
-        self.pos = self.x, self.y = pos
+    def __init__(self, position):
+        self.position = position
+
+    def look_away(self, additional_coords):
+        angle = math_operations.calculate_angle(*self.position, *additional_coords)
+        return angle
 
 
-class Player(Character):
-    pass
+class CharacterSprite(pygame.sprite.GroupSingle):
+    def __init__(self, image_name):
+        super().__init__()
+        char_sprite = pygame.sprite.Sprite()
+        char_sprite.rect = pygame.Rect(0, 0, 80, 80)
+        char_sprite.image = pygame.transform.scale(
+            pygame.image.load(image_name),
+            char_sprite.rect.size)
+        self.add(char_sprite)
+
+
+class PlayerCharacter(Character):
+    def __init__(self, position):
+        super().__init__(position)
+        self._sprite = CharacterSprite('character.png')
+
+    def draw(self, surface: pygame.Surface):
+        self._sprite = pygame.transform.rotate(self._sprite,
+                                               self.look_away(pygame.mouse.get_pos()))
+        self._sprite.draw(surface.subsurface(0, 0, 80, 80))
+
+    def move(self, direction: MoveDirection):
+        angle = self.look_away(pygame.mouse.get_pos())
+        if direction == MoveDirection.UP:
+            self.position = math_operations.change_position(
+                self.position, angle, constants.MAIN_CHAR_SPEED, 1)
+        elif direction == MoveDirection.DOWN:
+            self.position = math_operations.change_position(
+                self.position, angle, constants.MAIN_CHAR_SPEED, 2)
+        elif direction == MoveDirection.LEFT:
+            self.position = math_operations.change_position(
+                self.position, angle, constants.MAIN_CHAR_SPEED, 3)
+        else:
+            self.position = math_operations.change_position(
+                self.position, angle, constants.MAIN_CHAR_SPEED, 4)
 
 
 class NPC(Character):
@@ -12,8 +62,7 @@ class NPC(Character):
 
 
 class Item:
-    def __init__(self, pos):
-        self.pos = self.x, self.y = pos
+    pass
 
 
 class Weapon(Item):
@@ -21,8 +70,7 @@ class Weapon(Item):
 
 
 class Menu:
-    def __init__(self):
-        pass
+    pass
 
 
 class MainMenu(Menu):
@@ -34,5 +82,4 @@ class PauseMenu(Menu):
 
 
 class Map:
-    def __init__(self):
-        pass
+    pass
