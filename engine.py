192b68b32978635744efda7da1256c3fc79b1afb
vicebaraban <46491960+vicebaraban@users.noreply.github.com>
2021-01-05 13:06:31 +0300

    added engine file

---
+++ engine.py
@@ -0,0 +1,42 @@
+class Character:
+    pass
+
+
+class PlayerCharacter(Character):
+    pass
+
+
+class NonPlayerCharacter(Character):
+    pass
+
+
+class Enemy(NonPlayerCharacter):
+    pass
+
+
+class NeutralPerson(NonPlayerCharacter):
+    pass
+
+
+class Entity:
+    pass
+
+
+class Weapon(Entity):
+    pass
+
+
+class Menu:
+    pass
+
+
+class MainMenu(Menu):
+    pass
+
+
+class PauseMenu(Menu):
+    pass
+
+
+class Map:
+    pass
