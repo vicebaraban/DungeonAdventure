cf8223c09ce00af6a678f7154b4a04f2dec29bcd
vicebaraban <46491960+vicebaraban@users.noreply.github.com>
2021-01-06 23:19:07 +0300

    added vars for bullet

--- data.py
+++ data.py
@@ -5,8 +5,10 @@ from loading import load_image
 
 FPS = 60
 RESOLUTION = WIDTH, HEIGHT = 800, 600
-MAIN_CHAR_SPEED = 20
-NPC_SPEED = 18
+MAIN_CHAR_SPEED = 150
+NPC_SPEED = 120
+BULLET_SPEED = 350
+BULLET_TIME = 10
 DATA_PATH = 'textures'
 tile_size = tile_width, tile_height = 50, 50
 
