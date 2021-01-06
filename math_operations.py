214fcf010894003e32da6f557613ae33443dc210
vicebaraban <46491960+vicebaraban@users.noreply.github.com>
2021-01-06 15:22:06 +0300

    upd calculate_angle func

--- math_operations.py
+++ math_operations.py
@@ -2,19 +2,30 @@ import math
 
 
 def coord_quarter(x1, y1, x2, y2):
-    if x2 >= x1 and y2 <= y1:
+    if x2 > x1 and y2 < y1:
         return 1
-    elif x2 <= x1 and y2 <= y1:
+    elif x2 < x1 and y2 < y1:
         return 2
-    elif x2 <= x1 and y2 >= y1:
+    elif x2 < x1 and y2 > y1:
         return 3
-    return 4
+    elif x2 > x1 and y2 > y1:
+        return 4
+    return 0
 
 
 def calculate_angle(x1, y1, x2, y2):
     quarter = coord_quarter(x1, y1, x2, y2)
     len_x, len_y = x2 - x1, y1 - y2
-    if quarter == 1:
+    if not quarter:
+        if len_y == 0 and len_x > 0:
+            angle = 90
+        elif len_y == 0 and len_x < 0:
+            angle = 270
+        elif len_x == 0 and len_y < 0:
+            angle = 180
+        else:
+            angle = 0
+    elif quarter == 1:
         angle = int(math.degrees(math.atan(len_x / len_y)))
     elif quarter == 2:
         angle = 270 - int(math.degrees(math.atan(len_x / len_y)))
