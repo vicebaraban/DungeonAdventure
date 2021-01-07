2bac117c7e5bd530e3c57c9439fef25a5c3aafea
vicebaraban <46491960+vicebaraban@users.noreply.github.com>
2021-01-07 22:06:11 +0300

    added button conf

--- data.py
+++ data.py
@@ -13,6 +13,7 @@ DATA_PATH = 'textures'
 tile_size = tile_width, tile_height = 50, 50
 
 images = {'player': pygame.transform.scale(load_image('character.png'), tile_size),
-          'arrow': pygame.transform.scale(load_image('arrow.png'), (30, 8)),
+          'arrow': pygame.transform.scale(load_image('arrow.png'), (8, 10)),
           'bow': pygame.transform.scale(load_image('bow.png'), (10, 30)),
-          'sword': pygame.transform.scale(load_image('sword.png'), (30, 30))}
+          'sword': pygame.transform.scale(load_image('sword.png'), (30, 30)),
+          'button': pygame.transform.scale(load_image('button.png'), (30, 30))}
