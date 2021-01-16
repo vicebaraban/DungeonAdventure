0ff552a717a94ab0d905ad544cd7da1f209d2b90
vicebaraban <46491960+vicebaraban@users.noreply.github.com>
2021-01-16 17:25:25 +0300

    resize some objects

--- data.py
+++ data.py
@@ -13,14 +13,17 @@ MAP_PATH = 'maps'
 tile_size = tile_width, tile_height = 50, 50
 
 images = {'player': pygame.transform.scale(loading.load_image('character.png'), (tile_width - 5, tile_height - 5)),
-          'arrow': pygame.transform.scale(loading.load_image('arrow.png'), (8, 10)),
-          'bow': pygame.transform.scale(loading.load_image('bow.png'), (10, 30)),
+          'arrow': pygame.transform.scale(loading.load_image('arrow.png'), (9, 28)),
+          'bow': pygame.transform.scale(loading.load_image('bow.png'), (40, 15)),
           'sword': pygame.transform.scale(loading.load_image('sword.png'), (30, 30)),
-          'button': pygame.transform.scale(loading.load_image('button.png'), (30, 30)),
           'empty': pygame.transform.scale(loading.load_image('empty.png'), tile_size),
           'wall': pygame.transform.scale(loading.load_image('wall.png'), tile_size),
           'floor': pygame.transform.scale(loading.load_image('floor.png'), tile_size),
           'enemy': pygame.transform.scale(loading.load_image('enemy.gif'), (tile_width - 5, tile_height - 5)),
           'key': pygame.transform.scale(loading.load_image('key.png'), (40, 40)),
           'door_closed': pygame.transform.scale(loading.load_image('door_closed.png'), tile_size),
-          'door_opened': pygame.transform.scale(loading.load_image('door_opened.png'), tile_size)}
+          'door_opened': pygame.transform.scale(loading.load_image('door_opened.png'), tile_size),
+          'b_start_game': pygame.transform.scale(loading.load_image('b_start_game.png'), (100, 50)),
+          'b_exit': pygame.transform.scale(loading.load_image('b_exit.png'), (100, 50)),
+          'b_continue': pygame.transform.scale(loading.load_image('b_continue.png'), (100, 50)),
+          'b_back_to_menu': pygame.transform.scale(loading.load_image('b_back_to_menu.png'), (100, 50))}
